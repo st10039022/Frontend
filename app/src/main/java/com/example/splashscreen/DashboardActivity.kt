8053bd92478245d44b65c09065419a18e90425d9
st10039022 <st10039022@vcconnect.edu.za>
2025-08-08 13:23:23 +0200

    Donation page

--- app/src/main/java/com/example/splashscreen/DashboardActivity.kt
+++ app/src/main/java/com/example/splashscreen/DashboardActivity.kt
@@ -1,11 +1,19 @@
 package com.example.splashscreen
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        val donateButton = findViewById<ImageView>(R.id.buttonDonateDashboard)
+        donateButton.setOnClickListener {
+            val intent = Intent(this, DonateActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
